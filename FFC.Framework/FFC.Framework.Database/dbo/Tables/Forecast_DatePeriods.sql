32f2a220ba6ee4d3dc2dc72b0df802311caa3410
ashenfern <ashenfern@gmail.com>
2015-10-29 11:25:26 +0530

    Checking changes + Adding new tables

---
+++ FFC.Framework/FFC.Framework.Database/dbo/Tables/Forecast_DatePeriods.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Forecast_DatePeriods] (
+    [DatePeriodID] INT            IDENTITY (1, 1) NOT NULL,
+    [DatePeriod]   NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([DatePeriodID] ASC)
+);
+
---
+++ FFC.Framework/FFC.Framework.Database/dbo/Tables/Forecast_Methods.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Forecast_Methods] (
+    [ForecastMethodID]   INT            IDENTITY (1, 1) NOT NULL,
+    [ForecastMethod]     NVARCHAR (MAX) NULL,
+    [ForecastIdentifier] NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([ForecastMethodID] ASC)
+);
+

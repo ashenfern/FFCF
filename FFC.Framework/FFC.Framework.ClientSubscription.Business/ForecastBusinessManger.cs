42a618d68f4a61b5f5f4b35bf9d95ccf39cc637c
ashenfern <ashenfern@gmail.com>
2015-12-25 18:07:43 +0530

    checking changes + adding new changes

--- FFC.Framework/FFC.Framework.ClientSubscription.Business/ForecastBusinessManger.cs
+++ FFC.Framework/FFC.Framework.ClientSubscription.Business/ForecastBusinessManger.cs
@@ -1,4 +1,5 @@
-﻿using FFC.Framework.Data;
+﻿using FFC.Framework.ClientSubscription.ServiceFacade;
+using FFC.Framework.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +10,9 @@ namespace FFC.Framework.ClientSubscription.Business
 {
     public class ForecastBusinessManger
     {
-        WebServiceConnector<ForecastResult> service = new WebServiceConnector<ForecastResult>();
-
         public ForecastResult GetForecastResults(ForecastSearchCriteria searchCriteria)
         {
-            int branchId = searchCriteria.BranchId;
-            int productId = searchCriteria.ProductId;
-            string method = searchCriteria.Method.ForecastIdentifier.ToString();
-            string dataType = searchCriteria.DatePeriod.DatePeriod.ToString();
-            int periods = searchCriteria.ForecastPeriod;
-
-            //service.Resource = "api/Forecast/ForecastByMethod/1";
-            service.Resource = String.Format("api/Forecast/ForecastByMethod/{0}/{1}/{2}/{3}/{4}", branchId,productId, method, dataType, periods);
-            var result = service.GetData();
-            return result;
+            return new ForecastServiceFacade().GetForecastResults(searchCriteria);
         }
     }
 }

eea99911d03d052b69c3bf54eeaae6b95e50de66
ashenfern <ashenfern@gmail.com>
2016-01-26 15:51:27 +0530

    formatting the code

--- FFC.Framework/FFC.Framework.WebServicesManager/Utility/BranchDistributionData.cs
+++ FFC.Framework/FFC.Framework.WebServicesManager/Utility/BranchDistributionData.cs
@@ -9,7 +9,6 @@ namespace FFC.Framework.WebServicesManager.Utility
     public class BranchDistributionData
     {
         public int BranchId { get; set; }
-
         public string BranchName { get; set; }
         public int Amount { get; set; }
     }
